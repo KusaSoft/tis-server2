7dfb073df13c024a63eb0c2f5c5b70836fb36444
mauricio <madavaing@gmail.com>
2022-06-21 10:24:51 -0400

    endpoint notifications/{user_id} devuelve rejecteds, assigneds y confirmeds

--- routes/api.php
+++ routes/api.php
@@ -851,7 +851,7 @@ Route::get('reservations/rejected/{user_id}',function($user_id){
 
 Route::get('notifications/{user_id}',function($user_id){
     $reservations = UserBooking::where('user_id', $user_id)->where(function($q){
-        $q->where('state','assigned')->orWhere('state','rejected');
+        $q->where('state','assigned')->orWhere('state','rejected')->orWhere('state','confirmed');
     })->get();
     return $reservations->map(function ($elem) {
         $user = User::find($elem->user_id)->name;
