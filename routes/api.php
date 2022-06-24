928a7fa3941c3fe99e0794e0f59fa0c4c8fac093
mauricio <madavaing@gmail.com>
2022-06-23 20:56:48 -0400

    endpoint reservations/ put setea notification_date

--- routes/api.php
+++ routes/api.php
@@ -627,8 +627,8 @@ Route::get('classrooms/{userbooking_id}', function ($userbooking_id) {
         array_push($classrooms_id,$classroom["id"]);
     }
     $day = $reservation->reservation_date;
-    $hour_ini = $reservation->horario_ini;
-    $hour_end = $reservation->horario_end;
+    $x = $reservation->horario_ini;
+    $y = $reservation->horario_end;
     $classrooms_used = [];
     //             en un futuro cambiara a      'confirmed'
     $reservations = UserBooking::where('state', 'assigned')->where('reservation_date', $day)->get();
@@ -639,15 +639,16 @@ Route::get('classrooms/{userbooking_id}', function ($userbooking_id) {
         $horario_ini_time = strtotime($horario_ini);
         $horario_end_time = strtotime($horario_end);
         
-        $hour_ini_time = strtotime($hour_ini);
-        $hour_end_time = strtotime($hour_end);
+        $xx = strtotime($x);
+        $yy = strtotime($y);
         //si hay un solape en los horarios, lo anhadimos 
-        if(($horario_end_time > $hour_ini_time && $horario_end_time < $hour_end_time) ||
-           ($horario_ini_time > $hour_ini_time && $horario_ini_time < $hour_end_time) ||
-           ($horario_ini_time < $hour_ini_time && $horario_end_time > $hour_end_time)){
+        if(($horario_end_time > $xx && $horario_end_time < $yy) ||
+           ($horario_ini_time > $xx && $horario_ini_time < $yy) ||
+           ($horario_ini_time < $xx && $horario_end_time > $yy)){
             array_push($classrooms_used,$reserv->id);
         }
     }
+    return $classrooms_used;
     $classrooms_allowed = [];
     foreach($classrooms_id as $classroom){
         if(!in_array($classrooms_id,$classrooms_used)){
@@ -686,6 +687,11 @@ Route::put('reservations', function (Request $request) {
 
     $assigned_classrooms_str = implode(" ",$request->assigned_classrooms);
     $res->assigned_classrooms = $assigned_classrooms_str;
+
+
+    date_default_timezone_set("America/La_Paz");
+    $res->notification_date = date('Y-m-d H:i:s');
+
     $res->save();
     return response()->json([
         "message" => "...",
