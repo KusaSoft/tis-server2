58468c353ce30bbd69ba4e999494aab67fccd2ff
mauricio <madavaing@gmail.com>
2022-07-05 21:58:43 -0400

    reservas verificar si solicitud ya fue rechazada o asignada

--- routes/api.php
+++ routes/api.php
@@ -796,6 +796,16 @@ Route::put('reservations', function (Request $request) {
     $id = $request->id;
     $res = UserBooking::find($id);
 
+    if($res->state == 'assigned'){
+        return response()->json([
+            "message" => "Esta solicitud ya fue asignada"
+        ]);
+    }
+    if($res->state == 'rejected'){
+        return response()->json([
+            "message" => "Esta solicitud ya fue rechazada"
+        ]);
+    }
     $res->state = $request->state;
     $res->rejection_reason = $request->rejection_reason;
 
@@ -989,7 +999,7 @@ Route::get('notifications/{user_id}', function ($user_id) {
     return $reservations->map(function ($elem) {
         $user = User::find($elem->user_id)->name;
         $subject = Subject::find($elem->subject_id)->name_subject;
-        $name_subject = $subject->name;
+        $name_subject = $subject->name_subject;
         $assigned_classrooms = [];
         $assigned_classrooms_str  = explode(" ", $elem->assigned_classrooms);
         $classrooms = "Se asigno las aulas: ";
